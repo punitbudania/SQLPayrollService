e9faf9edd7f7c6ce8d96efcedb62c17a9bc0bc7f
punitbudania <punitbudania88@gmail.com>
2020-12-18 16:42:41 +0530

    [punit]Added: created employee_payroll table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -2,3 +2,13 @@ UC_1
 CREATE DATABASE payroll_service_db;
 SHOW DATABASES;
 USE payroll_service_db;
+
+UC_2
+CREATE TABLE employee_payroll
+(
+ id INT unsigned NOT NULL AUTO_INCREMENT,
+ name VARCHAR(150) NOT NULL,
+ salary DOUBLE NOT NULL,
+ start DATE NOT NULL,
+ PRIMARY KEY (id)
+);
