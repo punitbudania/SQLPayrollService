f9a273a1b380ea869fd1bbd89ebb3d7197c35c6a
punitbudania <punitbudania88@gmail.com>
2020-12-21 16:29:44 +0530

    [punit]Added: created er diagram

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -50,4 +50,34 @@ UC_10
 INSERT INTO employee_payroll (id, name, phone_number, address, department, gender, basic_pay, deductions, taxable_pay, tax, net_pay, start)
 VALUES (4, 'Terisa', NULL, 'TBD', 'Marketing', 'F', 3000000.0, 1000000.0, 2000000.0, 500000.0, 1500000.0, '2018-01-03');
 
-
+UC_11
+CREATE TABLE company
+(
+ company_id INT NOT NULL AUTO_INCREMENT,
+ company_name VARCHAR(150),
+ PRIMARY KEY(company_id)
+);
+CREATE TABLE department
+( 
+ department_id INT NOT NULL AUTO_INCREMENT,
+ department_name VARCHAR(150),
+ PRIMARY KEY(department_id)
+);
+CREATE TABLE payroll
+(
+ employee_id INT unsigned NOT NULL AUTO_INCREMENT,
+ basic_pay INT,
+ deductions INT,
+ taxable_pay INT,
+ tax INT,
+ net_pay INT,
+ FOREIGN KEY(employee_id) REFERENCES employee(employee_id) ON DELETE CASCADE
+);
+CREATE TABLE employee_department
+(
+ department_id INT NOT NULL,
+ employee_id INT unsigned NOT NULL,
+ FOREIGN KEY(department_id) REFERENCES department(department_id) ON DELETE CASCADE,
+ FOREIGN KEY(employee_id) REFERENCES employee(employee_id) ON DELETE CASCADE
+);
+ALTER TABLE employee ADD FOREIGN KEY (company_id) REFERENCES company(company_id) ON DELETE CASCADE;
